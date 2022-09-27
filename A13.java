ecd76d2f81c40f72564e604365412929b7352f9b
sandhyaMahalkar <114503392+sandhyaMahalkar@users.noreply.github.com>
2022-09-27 20:54:43 +0530

    Create A13.java

---
+++ A13.java
@@ -0,0 +1,18 @@
+//Question 13)Write a program to demostrating autoboxing of float variable
+//Code:-
+class D {
+
+    public static void main(String[] args) {
+
+        Float f = new Float( 50);
+
+        float f1 = f;
+        System.out.println("value of f: "+ f);
+        System.out.println("Value of f1: "+f1);
+    }
+}
+/*
+Outpute:-
+value of f: 50.0
+Value of f1: 50.0
+*/
