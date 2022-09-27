2d25658ff4204e55f7c75d6b49c0465f4a095ec9
sandhyaMahalkar <114503392+sandhyaMahalkar@users.noreply.github.com>
2022-09-27 20:56:09 +0530

    Create A15.java

---
+++ A15.java
@@ -0,0 +1,16 @@
+//Question 15)Write a program to demonstrating exesplite type casting in java
+//Code:- 
+class D {
+    public static void main(String[] args) {
+        int i = 50;
+        System.out.println("The integer value: " + i);
+
+        double d = i;
+        System.out.println("The double value: " + d);
+    }
+}
+/*
+Output:-
+The integer value: 50
+The double value: 50.0
+*/
