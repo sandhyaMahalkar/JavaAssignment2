d192c04c92c82858e2e641e52877d684efb88515
sandhyaMahalkar <114503392+sandhyaMahalkar@users.noreply.github.com>
2022-09-27 20:55:51 +0530

    Create A14.java

---
+++ A14.java
@@ -0,0 +1,19 @@
+//Question 14)Write a program to demonstrating autoboxing of double variable
+//Code:-
+class D {
+
+    public static void main(String[] args) {
+
+        Double d = new Double( 60);
+
+        double d1 = d;
+        System.out.println("value of f: "+ d);
+        System.out.println("Value of f1: "+d1);
+    }
+}
+
+/*
+Outpute:-
+value of f: 60.0
+Value of f1: 60.0
+*/
