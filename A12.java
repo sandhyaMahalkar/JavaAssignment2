fedca7cb013e374535e6ccaf03b177fd04961c10
sandhyaMahalkar <114503392+sandhyaMahalkar@users.noreply.github.com>
2022-09-27 20:54:08 +0530

    Create A12.java

---
+++ A12.java
@@ -0,0 +1,22 @@
+//Question 12)Write a program to demonstrating autoboxing of intiger variavle
+//Code:-
+class D {
+    
+    public static void main(String[] args) {
+
+        Integer i = new Integer(10);
+
+
+        int i1 = i;
+
+        System.out.println("Value of i:" + i);
+        System.out.println("Value of i1: " + i1);
+    }
+}
+/*
+Output:-
+Value of i:10
+Value of i1: 10
+
+Process finished with exit code 0
+*/
